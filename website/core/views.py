1df91b0aaf60449543538861c366643b6d67bdb4
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-15 17:11:05 +0330

    adding all the blog related stuff

--- website/core/views.py
+++ website/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    blog_item = Blog.objects.all()
+    return render(request , "index.html" , {"Blogs": blog_item })
