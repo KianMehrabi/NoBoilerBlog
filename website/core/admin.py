62d38b66f3dd8a8f667f8c917514328855029350
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-16 16:28:48 +0330

    fixing some problem

--- website/core/admin.py
+++ website/core/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Blog
 
-admin.site.register(Blog)
+@admin.register(Blog)
+class BlogPostAdmin(admin.ModelAdmin):
+    pass
--- website/core/models.py
+++ website/core/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
     
     class Meta:
         verbose_name_plural = "Blogs"
     
     def __str__(self):
         return self.title
-        
-    
