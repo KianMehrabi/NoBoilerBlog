a4c339cbb4753900c204f993e5bc0776fba6b7ab
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-16 12:48:47 +0330

    adding blog detail and 404 page handeler

--- website/core/views.py
+++ website/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Blog
+from django.http import HttpResponseNotFound
 
 # Create your views here.
-def index(request):
+def index(request , *args , **kwargs):
     blog_item = Blog.objects.all()
     return render(request , "index.html" , {"Blogs": blog_item })
+
+def individual_blog(request , id):
+    try:
+        blog_item = Blog.objects.get(id = id)
+    except:
+        print("can not found the data")
+        return HttpResponseNotFound("did not found the page you were looking for")
+    return render(request , "blog.html" , {"Blog": blog_item })
--- website/website/urls.py
+++ website/website/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , index , name="home"),  
+    path('' , views.index , name="home"),  
+    path('Blogs/<int:id>/' , views.individual_blog),  
 ]
